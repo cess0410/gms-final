a036d62e32ee8a56f4a8bac75c311b2b4a12ca19
Clarence Baluyot <akingprincess@gmail.com>
2024-07-02 18:51:07 +0800

    add checks

--- api/schedule_action.php
+++ api/schedule_action.php
@@ -9,29 +9,30 @@ extract($_POST);
 $allday = isset($allday);
 // process start time and emd time using $start_datetime and $end_datetime
 // if (empty($id)) {
-$sql = "SELECT id FROM schedule_list WHERE doctor = ? AND specialty = ? AND ((start_datetime <= ? AND end_datetime >= ?) OR (start_datetime <= ? AND end_datetime >= ?))";
-$stmt = $db->prepare($sql);
-$stmt->bind_param("ssssss", $doctor, $specialty, $end_datetime, $start_datetime, $end_datetime, $start_datetime);
-$stmt->execute();
-$stmt->store_result();
-$conflict = $stmt->num_rows() > 0;
+if (strtotime($start_datetime) > strtotime($end_datetime)) {
+    echo "<script> alert('Start time should be lesser than End time.'); location.replace('./') </script>";
+    $db->close();
+    exit;
+}
+$conflict = $db->query("SELECT id FROM schedule_list WHERE doctor = '$doctor' AND specialty = '$specialty' AND ((start_datetime <= '$end_datetime' AND end_datetime >= '$start_datetime') OR (start_datetime <= '$start_datetime' AND end_datetime >= '$end_datetime'))")->num_rows > 0;
 
-if (!$conflict) {
-    $insert_sql = "INSERT INTO `schedule_list` (`doctor`, `specialty`, `start_datetime`, `end_datetime`) VALUES (?, ?, ?, ?)";
-    $insert_stmt = $db->prepare($insert_sql);
-    $insert_stmt->bind_param("ssss", $doctor, $specialty, $start_datetime, $end_datetime);
-
-
-    if (!$insert_stmt->execute()) {
+if ($conflict) {
+    echo "<script> alert('Schedule conflicts with existing Schedule.'); window.location.href = '../manage_schedule.php';</script>";
+    $db->close();
+    exit;
+}else{
+    $insert_sql = "INSERT INTO `schedule_list` (`doctor`, `specialty`, `start_datetime`, `end_datetime`) VALUES ('$doctor', '$specialty', '$start_datetime', '$end_datetime')";
+    $save = $db->query($insert_sql);
+    if ($save) {
+        echo "<script> alert('Schedule Successfully Saved.'); window.location.href = '../manage_schedule.php';</script>";
+    } else {
         echo "<pre>";
-        echo "An error occurred.<br>";
-        echo "Error: " . $insert_stmt->error . "<br>";
-        echo "SQL: " . $insert_sql . "<br>";
+        echo "An Error occured.<br>";
+        echo "Error: " . $db->error . "<br>";
+        echo "SQL: " . $insert_sql  . "<br>";
         echo "</pre>";
     }
-} else {
 
-    echo "<script> alert('Schedule conflicts with existing Schedule.'); window.location.href = '../manage_schedule.php';</script>";
 }
 // } else {
 //     $sql = "UPDATE `schedule_list` set `doctor` = ?, `specialty` = ?, `start_datetime` = ?, `end_datetime` = ? where `id` = ?";
@@ -39,15 +40,5 @@ if (!$conflict) {
 //     $stmt->bind_param("sssss", $doctor, $specialty, $start_datetime, $end_datetime, $id);
 //     $stmt->execute();
 // }
-$save = $db->query($sql);
-if ($save) {
-    echo "<script> alert('Schedule Successfully Saved.'); window.location.href = '../manage_schedule.php'; </script>";
-} else {
-    echo "<pre>";
-    echo "An Error occured.<br>";
-    echo "Error: " . $db->error . "<br>";
-    echo "Error: " . $db->error . "<br>";
-    echo "SQL: " . $sql . "<br>";
-    echo "</pre>";
-}
+
 $db->close();
