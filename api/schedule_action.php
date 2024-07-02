19b8aeed646d555178fa9ddca0250f15993c63bb
Clarence Baluyot <akingprincess@gmail.com>
2024-07-02 18:40:12 +0800

    Schedule Fix

--- api/schedule_action.php
+++ api/schedule_action.php
@@ -8,11 +8,37 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 extract($_POST);
 $allday = isset($allday);
 // process start time and emd time using $start_datetime and $end_datetime
-if (empty($id)) {
-    $sql = "INSERT INTO `schedule_list` (`doctor`,`specialty`,`start_datetime`, `end_datetime`) VALUES ('$doctor','$specialty','$start_datetime', '$end_datetime')";
+// if (empty($id)) {
+$sql = "SELECT id FROM schedule_list WHERE doctor = ? AND specialty = ? AND ((start_datetime <= ? AND end_datetime >= ?) OR (start_datetime <= ? AND end_datetime >= ?))";
+$stmt = $db->prepare($sql);
+$stmt->bind_param("ssssss", $doctor, $specialty, $end_datetime, $start_datetime, $end_datetime, $start_datetime);
+$stmt->execute();
+$stmt->store_result();
+$conflict = $stmt->num_rows() > 0;
+
+if (!$conflict) {
+    $insert_sql = "INSERT INTO `schedule_list` (`doctor`, `specialty`, `start_datetime`, `end_datetime`) VALUES (?, ?, ?, ?)";
+    $insert_stmt = $db->prepare($insert_sql);
+    $insert_stmt->bind_param("ssss", $doctor, $specialty, $start_datetime, $end_datetime);
+
+
+    if (!$insert_stmt->execute()) {
+        echo "<pre>";
+        echo "An error occurred.<br>";
+        echo "Error: " . $insert_stmt->error . "<br>";
+        echo "SQL: " . $insert_sql . "<br>";
+        echo "</pre>";
+    }
 } else {
-    $sql = "UPDATE `schedule_list` set `doctor` = '{$doctor}', `specialty` = '{$specialty}', `start_datetime` = '{$start_datetime}', `end_datetime` = '{$end_datetime}' where `id` = '{$id}'";
+
+    echo "<script> alert('Schedule conflicts with existing Schedule.'); window.location.href = '../manage_schedule.php';</script>";
 }
+// } else {
+//     $sql = "UPDATE `schedule_list` set `doctor` = ?, `specialty` = ?, `start_datetime` = ?, `end_datetime` = ? where `id` = ?";
+//     $stmt = $db->prepare($sql);
+//     $stmt->bind_param("sssss", $doctor, $specialty, $start_datetime, $end_datetime, $id);
+//     $stmt->execute();
+// }
 $save = $db->query($sql);
 if ($save) {
     echo "<script> alert('Schedule Successfully Saved.'); window.location.href = '../manage_schedule.php'; </script>";
